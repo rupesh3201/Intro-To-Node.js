f5545cdf1363d996f8dfe81f5adf6b37fe232587
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-04 16:10:45 +0530

    alert is not defiend in node js causes it decleared inside the broswer

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const b = 10;
 const sum = a + b;
 console.log(`Sum:${sum}`);
 // in node js the its does not required any script body ony node logi will retrun 
+//alert is not defiend in node js causes it decleared inside the broswer
