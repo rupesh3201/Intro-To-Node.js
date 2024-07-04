948247e78eab4d3c3f4001d70391fc274244a457
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-04 16:11:12 +0530

    promt is not allowed

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const sum = a + b;
 console.log(`Sum:${sum}`);
 // in node js the its does not required any script body ony node logi will retrun 
 //alert is not defiend in node js causes it decleared inside the broswer
+//promt is not allowed
+//
