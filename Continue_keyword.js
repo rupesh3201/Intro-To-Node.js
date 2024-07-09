e19726d79ce1bd0c19ef783a4a296747169bdee3
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-09 18:12:48 +0530

    Continue  will skip the ittreation

--- Continue_keyword.js
+++ Continue_keyword.js
@@ -1,8 +1,8 @@
 for(let i=1; i<=10; i++)
     {
-        if(i==5)
+        if(i==5 || i==7)//it simply skip the itretaion 
         {
             continue;//by using continue  keyword  loop  will jump over condtion then we use continue  
         }
-        console.log(`printing page ${i}`)
+        console.log(`Hello ${i}`)
     }
