dc5fd9a058db4739020fb9511699d5095f0f3009
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-13 20:40:12 +0530

    setTimeout

--- setTimeOut.js
+++ setTimeOut.js
@@ -1,6 +1,10 @@
 function sayhii() // its also knwon as callback function
 {
-    console.log("say hello")
+   const mydate = new Date();
+   const minutes = mydate.getMinutes();
+   const  Seconds = mydate.getSeconds();
+   console.log(`minutes:${minutes} Seconds:${minutes} `);// 
+
 }
 setTimeout(sayhii,4000) // higerorder funcion
 setInterval(sayhii,2000)// after blink
