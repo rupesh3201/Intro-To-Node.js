503c0f1fbdb16f603a23d21680d5569502a37d86
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-13 20:06:43 +0530

    C:/Program Files/Git/ its gives months

--- Date_class.js
+++ Date_class.js
@@ -3,6 +3,7 @@ const mydate = new Date();
     console.log(mydate )//printing output its print time  in IST  , GMT greenmeaantime(universally accpted time)Ut
     console.log(mydate.toLocaleDateString() ) //it's give local gate 
     console.log(mydate.toLocalString ())// its gives total time
+    console.log(mydate.getMonth()+1 );//its give months
 
 
 }
