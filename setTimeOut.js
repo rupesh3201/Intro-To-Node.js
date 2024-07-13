8f75e17aa8aff02cc59bf8ffa055b6fd38511aeb
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-13 20:40:30 +0530

    setInterval

--- setTimeOut.js
+++ setTimeOut.js
@@ -3,7 +3,7 @@ function sayhii() // its also knwon as callback function
    const mydate = new Date();
    const minutes = mydate.getMinutes();
    const  Seconds = mydate.getSeconds();
-   console.log(`minutes:${minutes} Seconds:${minutes} `);// 
+   console.log(`minutes:${minutes} Seconds:${minutes} `);
 
 }
 setTimeout(sayhii,4000) // higerorder funcion
