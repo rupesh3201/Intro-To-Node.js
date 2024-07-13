b1b189535940793f7000e7264fe6556724d17a58
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-13 20:01:03 +0530

    loacal date

--- Date_class.js
+++ Date_class.js
@@ -1,4 +1,6 @@
 const mydate = new Date();
 {
     console.log(mydate )//printing output its print time  in IST  , GMT greenmeaantime(universally accpted time)Ut
+    console.log(mydate.toISOString ) //it's give local gate 
+
 }
