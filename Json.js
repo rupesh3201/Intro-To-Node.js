161eea851c4fa269e299845eb38b3202644ac6df
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-13 19:31:10 +0530

    adress

--- Json.js
+++ Json.js
@@ -2,4 +2,7 @@ const User_details = //josh
 {
     "User_id": 1, //int
     "Name": "Rupesh", //string
-   
+    "Age": 21, //Number
+    "Mo_n": 9021976854,
+    "Adress":  // We can store object itself
+    
