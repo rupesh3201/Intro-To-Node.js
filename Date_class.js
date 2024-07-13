d4555fe574ceebe71b5dc580fd992c39f3f356ff
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-07-13 20:05:12 +0530

    C:/Program Files/Git/ its gives total time

--- Date_class.js
+++ Date_class.js
@@ -1,6 +1,8 @@
 const mydate = new Date();
 {
     console.log(mydate )//printing output its print time  in IST  , GMT greenmeaantime(universally accpted time)Ut
-    console.log(mydate.toISOString ) //it's give local gate 
+    console.log(mydate.toLocaleDateString() ) //it's give local gate 
+    console.log(mydate.toLocalString ())// its gives total time
+
 
 }
